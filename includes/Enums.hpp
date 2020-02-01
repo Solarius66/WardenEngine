b2a2ebc0f591e747a800ad2bf44a488881f15b03
Thénard Marc-Antoine <Sora@localhost.localdomain>
2020-02-01 01:24:24 +0100

    [ADD] Enums.hpp, header file dor LAYER and ID enum declaration

---
+++ includes/Enums.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2020
+** Enums
+** File description:
+** LAYER and ID enum declaration
+*/
+
+#ifndef ENUMS_HPP
+#define ENUMS_HPP
+
+namespace wd
+{
+    // Layer enum declaration for display order, could be HUD, FOREGROUND or BACKGROUND
+    enum LAYER {
+        HUD,
+        FOREGROUND,
+        BACKGROUND
+    };
+
+    // ID enum declaration for object type definition, could be NOTHING, PLAYER, ENEMY, ITEM or SYSTEM
+    enum ID {
+        NOTHING,
+        PLAYER,
+        ENEMY,
+        ITEM,
+        SYSTEM
+    };
+}
+
+#endif /* !ENUMS_HPP */
