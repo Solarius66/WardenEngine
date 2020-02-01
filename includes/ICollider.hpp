79edee1dd6105c7e5b7cf0516b5a7fc7ef520043
Thénard Marc-Antoine <Sora@localhost.localdomain>
2020-02-01 12:54:36 +0100

    [ADD] adding ICollider.hpp, header file of Collider class Inteface

---
+++ includes/ICollider.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2020
+** ICollider
+** File description:
+** Collider class Interface declaration
+*/
+
+#ifndef ICOLLIDER_HPP
+#define ICOLLIDER_HPP
+
+namespace wd
+{
+    class ICollider
+    {
+        public:
+            ICollider() = delete;
+            virtual ~ICollider();
+
+            // members getters
+            virtual bool    getIsCollide() const = 0;
+
+            // members setters
+            virtual void    setIsCollide(bool isCollide) = 0;
+
+            // trigger collider functions
+            virtual void    onTriggerEnter() = 0;
+            virtual void    onTriggerExit() = 0;
+            virtual void    onTriggerStay() = 0;
+
+            // update member function
+            virtual void    update() = 0;
+    };
+}
+
+#endif /* !ICOLLIDER_HPP */
