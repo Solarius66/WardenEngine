2246ea1c9f936de3053d578c5ace8e0d2ab073b1
Thénard Marc-Antoine <Sora@localhost.localdomain>
2020-02-01 14:29:10 +0100

    [ADD] Collider.hpp/cpp, Collider class declaration and implementation

---
+++ includes/Collider.hpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2020
+** Collider
+** File description:
+** Collider class declaration
+*/
+
+#ifndef COLLIDER_HPP
+#define COLLIDER_HPP
+
+#include <ostream>
+#include "ICollider.hpp"
+
+#pragma pack(push, 1)
+
+namespace wd
+{
+    class Collider : public wd::ICollider {
+        public:
+            Collider();
+            Collider(const wd::Collider &);
+            ~Collider();
+
+            // members getters
+            bool    getIsCollide() const {return _isCollide;}
+
+            // members setters
+            void    setIsCollide(const bool &);
+
+            // trigger collider functions
+            void    onTriggerEnter();
+            void    onTriggerExit();
+            void    onTriggerStay();
+
+            // update member function
+            void    update();
+
+            // overloads on operators
+            wd::Collider    &operator=(const wd::Collider &);
+
+        private:
+            bool    _isCollide;
+    };
+}
+
+#pragma pack(pop)
+
+std::ostream &operator<<(std::ostream &, const wd::Collider &);
+
+#endif /* !COLLIDER_HPP */
---
+++ src/Collider.cpp
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2020
+** Collider
+** File description:
+** Collider member functions implementation
+*/
+
+#include <iostream>
+#include <ostream>
+#include "Collider.hpp"
+
+wd::Collider::Collider()
+{
+}
+
+wd::Collider::Collider(const wd::Collider &ref) : _isCollide(ref._isCollide)
+{
+}
+
+wd::Collider::Collider::~Collider()
+{
+}
+
+// Set _isCollide from a bool passed as parameter
+
+void wd::Collider::setIsCollide(const bool &isCollide)
+{
+    _isCollide = isCollide;
+}
+
+// Trigger collider member functions
+
+void wd::Collider::onTriggerEnter()
+{
+}
+
+void wd::Collider::onTriggerExit()
+{
+}
+
+void wd::Collider::onTriggerStay()
+{
+}
+
+// Update Collider member function
+
+void wd::Collider::update()
+{
+}
+
+// Overload on = operator to assign ref members to the current Collider
+
+wd::Collider &wd::Collider::operator=(const wd::Collider &ref)
+{
+    _isCollide = ref._isCollide;
+    return *this;
+}
+
+// Overload << operator on std::cout to print _isCollide in alpha format of Collider
+
+std::ostream &operator<<(std::ostream &os, const wd::Collider &collider)
+{
+    return os << "isCollide: " << std::boolalpha << collider.getIsCollide();
+}
