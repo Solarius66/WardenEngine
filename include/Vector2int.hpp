c7c04579385eccaea9b6f6c9494aa17431e53fd4
Thénard Marc-Antoine <Sora@localhost.localdomain>
2020-02-02 14:28:42 +0100

    [MOD] changing Vector2int class to a struct

--- include/Vector2int.hpp
+++ include/Vector2int.hpp
@@ -2,38 +2,23 @@
 ** EPITECH PROJECT, 2020
 ** Vector2int
 ** File description:
-** Vector2int template class declaration
+** Vector2int structure of 2 integers declaration
 */
 
 #ifndef VECTOR2INT_HPP
 #define VECTOR2INT_HPP
 
-#include <ostream>
-
 #pragma pack(push, 1)
 
 namespace wd
 {
-    class Vector2int
+    typedef struct Vector2int_s
     {
-        public:
-            Vector2int(const int &xRef = 0, const int &yRef = 0);
-            Vector2int(const Vector2int &);
-            ~Vector2int();
-
-            // operator overload to assign memebers
-            Vector2int &operator=(const Vector2int &);
-
-            // public member
-            int x;
-            int y;
-    };
+        int x;
+        int y;
+    } Vector2int;
 }
 
 #pragma pack(pop)
-
-// overload << on std::cout to print x and y
-
-std::ostream &operator<<(std::ostream &, const wd::Vector2int &);
 
 #endif /* !VECTOR2INT_HPP */
